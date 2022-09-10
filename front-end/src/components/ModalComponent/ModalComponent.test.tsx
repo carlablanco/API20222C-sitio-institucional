370b14bdb5d27eb216c7a06949d2b508ca6bca2e
Sergio Garroni <serg2404@gmail.com>
2022-09-09 21:02:30 -0300

    modal profesor

--- front-end/src/components/ModalComponent/ModalComponent.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ModalComponent from './ModalComponent';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<ModalComponent />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
