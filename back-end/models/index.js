29ce3f4bc35038904f77d7ea35ba1c09f5606d8f
Carla <88563106+carlablanco@users.noreply.github.com>
2022-09-09 21:04:31 -0300

    random changes

--- back-end/models/index.js
+++ back-end/models/index.js
@@ -7,7 +7,7 @@ const basename = path.basename(__filename);
 const config = require('../config/database');
 const db = {};
 
-// Creamos nuestra conexión
+// Conexión a MySQL
 let sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 fs
