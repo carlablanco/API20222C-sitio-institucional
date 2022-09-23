e357af757e9d8d0a5881f0e8f2b10ced2be7f783
Carla <88563106+carlablanco@users.noreply.github.com>
2022-09-23 19:37:41 -0300

    vista profesor completa

--- front-end/src/services/login.js
+++ front-end/src/services/login.js
@@ -3,8 +3,10 @@ import axios from "axios";
 const baseUrl = 'http://localhost:4000/api/login'
 
 const login = async credentials => {
+    // implementar try
     const { data } = await axios.post(baseUrl, credentials)
     return data
+    // guardar de una en el session storage en vez de return data
 }
 
 export default { login }
