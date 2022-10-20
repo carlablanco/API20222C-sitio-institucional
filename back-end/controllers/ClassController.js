4494dc29e0d313d47fb2b352f9676d50010543ec
Carla <88563106+carlablanco@users.noreply.github.com>
2022-10-20 19:58:50 -0300

    new class controller bien piola

--- back-end/controllers/ClassController.js
+++ back-end/controllers/ClassController.js
@@ -1,18 +1,86 @@
+const db = require("../models");
+const Class = db.Class;
+const Op = db.Sequelize.Op;
 
-
-exports.CreateClass = async function (req, res, next) {
-    var Class = {
+exports.createClass = (req, res) => {
+    // Validate request
+    if (!req.body.name) {
+      res.status(400).send({
+        message: "La clase no puede estar vacía."
+      });
+      return;
+    }
+  
+    // Create a Class
+    const aClass = {
         professor: req.body.professor,
         name: req.body.name,
         duration: req.body.duration,
         frequency: req.body.frequency,
         type: req.body.type,
         cost: req.body.cost,
-        status: "No publicada" //que iria acá inicialmente? no publicada
-    }
-}
+        status: "No publicada"
+    };
+  
+    // Save Class in the database
+    Class.create(aClass)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Ocurrió un error"
+        });
+      });
+  };
 
 
-//si la clase arranca no publicada entonces necesitamos exports.ChangeClass
-//costo no negativo
-// fq enum
+  exports.updateClass = (req, res) => {
+    const id = req.params.id;
+  
+    Class.update(req.body, {
+      where: { id: id }
+    })
+      .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "La clase fue actualizada."
+          });
+        } else {
+          res.send({
+            message: `Error: La clase con id=${id} no existe.`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error actualizando la clase con id=" + id
+        });
+      });
+  };
+
+
+  exports.deleteClass = (req, res) => {
+    const id = req.params.id;
+  
+    Class.destroy({
+      where: { id: id }
+    })
+      .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "Se borró la clase"
+          });
+        } else {
+          res.send({
+            message: `La clase con id =${id} no existe.`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Ocurrió un error, no se pudo borrar la clase con id=" + id
+        });
+      });
+  };
