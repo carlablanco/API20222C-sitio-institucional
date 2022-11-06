57f07cc13a508bc944d36a7a7cc4951620b5eb49
Carla <88563106+carlablanco@users.noreply.github.com>
2022-11-05 21:48:18 -0300

    Update ClassCommentController.js

--- back-end/controllers/ClassCommentController.js
+++ back-end/controllers/ClassCommentController.js
@@ -2,23 +2,26 @@ const db = require("../models");
 const class_comment = db.class_comment;
 const Op = db.Sequelize.Op;
 
+
 exports.createComment = (req, res) => {
-    // Validate request
-    if (!req.body.content) {
-      res.status(400).send({
-        message: "El comentario no puede estar vacío."
-      });
-      return;
-    }
-  
+  // Validate request
+  if (!req.body.content) {
+    res.status(400).send({
+      message: "El comentario no puede estar vacío."
+    });
+    return;
+  }
+
+  const project = class_comment.findOne({ where: { id_class: req.body.id_class, id_student: req.body.id_student } });
+  if (project === null) {
     // Create 
     const aComment = {
-        id_class: req.body.id_class,
-        id_student: req.body.id_student,
-        content: req.body.content,
-        stars: req.body.stars
+      id_class: req.body.id_class,
+      id_student: req.body.id_student,
+      content: req.body.content,
+      stars: req.body.stars
     };
-  
+
     // Save in the database
     class_comment.create(aComment)
       .then(data => {
@@ -30,50 +33,57 @@ exports.createComment = (req, res) => {
             err.message || "Ocurrió un error"
         });
       });
-  };
+  } else {
+    project.content = req.body.content
+    project.stars = req.body.stars
+    project.save()
+  }
+  return res.send(project)
+};
+
 
 function sendMail(email, message) {
   var mailOptions = {
-      from: "noreply@culturetour.local",
-      to: email,
-      subject: "Tu comentario fue eliminado",
-      text: "El motivo fue:" + message
+    from: "noreply@culturetour.local",
+    to: email,
+    subject: "Tu comentario fue eliminado",
+    text: "El motivo fue:" + message
   };
 
   mailer.transport.sendMail(mailOptions)
 }
 
 
-  exports.deleteComment = (req, res) => {
-    const id = req.params.id;
-  
-    class_comment.destroy({
-      where: { id: id }
-    })
-      .then(num => {
-        if (num == 1) {
-          res.send({
-            message: "Se borró el comentario."
-          });
+exports.deleteComment = (req, res) => {
+  const id = req.params.id;
 
-          sendMail(req.body.mail, req.body.message)
-          
-        } else {
-          res.send({
-            message: `El comentario con id =${id} no existe.`
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).send({
-          message: "Ocurrió un error, no se pudo borrar el comentario con id=" + id
+  class_comment.destroy({
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Se borró el comentario."
         });
+
+        sendMail(req.body.mail, req.body.message)
+
+      } else {
+        res.send({
+          message: `El comentario con id =${id} no existe.`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Ocurrió un error, no se pudo borrar el comentario con id=" + id
       });
-  };
+    });
+};
 
 
-  exports.findAllComments = (req, res) => {
-    var conditions = []
+exports.findAllComments = (req, res) => {
+  var conditions = []
 
   if (req.body.id_class) {
     conditions.push({ name: req.body.id_class })
@@ -81,12 +91,14 @@ function sendMail(email, message) {
 
   if (req.body.id) {
     conditions.push({ type: req.body.id })
-  
-    class_comment.findAll({ where: conditions,
+
+    class_comment.findAll({
+      where: conditions,
       include: {
         as: 'student',
         model: db.sequelize.model('User')
-      } })
+      }
+    })
       .then(data => {
         res.send(data);
       })
@@ -96,4 +108,5 @@ function sendMail(email, message) {
             err.message || "Some error occurred while retrieving comments."
         });
       });
-  } };
+  }
+};
